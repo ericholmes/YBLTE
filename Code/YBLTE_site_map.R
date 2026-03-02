7f482c95f78976b41ba44b2d9813ae52f1812405
ericholmes <ejholmes@ucdavis.edu>
2026-03-02 10:44:22 -0800

    Updated site map

--- Code/YBLTE_site_map.R
+++ Code/YBLTE_site_map.R
@@ -6,13 +6,22 @@ library(tidyverse)
 library(ggsflabel)
 library(deltamapr)
 library(basemaps)
+library(nhdplusTools)
 library(ggrepel)
 library(ggspatial)
+library(readxl)
 
 API = T
 saveoutput = F
 # Data procurement ---------------------------------------------------------
-## Need to be plugged into DWR network or VPN
+
+# Read Excel file
+proj_raw <- read_excel("C:/Users/eholmes/Box/Yolo_Food_Web/Data/tabular/YBLTE_sites.xlsx")
+
+# Convert to sf points (WGS84)
+proj_pts <- proj_raw %>%
+  st_as_sf(coords = c("Lon", "Lat"), crs = 4326, remove = FALSE)
+
 if(API){
   ## Download Flood bypass spatial data ----
   flood_bypasses_2014_url <- "https://gis.data.cnra.ca.gov/api/download/v1/items/5d56a0c6d8414b29a4769c0c4fbe8536/geojson?layers=0"
@@ -31,18 +40,34 @@ if(API){
   # Define the NHD API endpoint
   url <- "https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer/6/query"
   
-  # Set query parameters to filter for Feather River
-  query_params <- list(
-    where = "GNIS_NAME='Feather River'",
-    outFields = "*",
-    f = "geojson"
+  # Sacramento Valley bounding box (xmin, ymin, xmax, ymax)
+  sac_bbox <- st_bbox(c(
+    xmin = -122.2,
+    ymin = 38.0,
+    xmax = -121.0,
+    ymax = 39.3
+  ), crs = 4326)
+  
+  # Convert bbox to sf polygon
+  sac_poly <- st_as_sfc(sac_bbox)
+  
+  
+  # Download NHDPlus flowlines within the bbox
+  flowlines <- get_nhdplus(
+    sac_poly,
+    realization = "flowline",
+    streamorder = 3,        # only larger streams (adjust as needed)
+    t_srs = 4326
   )
+
+  major_names <- c("Sacramento River",
+                   "Feather River",
+                   "American River",
+                   "Putah Creek",
+                   "Yuba River")
   
-  # Request feather river data from the API
-  response <- GET(url, query = query_params)
-  
-  # Read the feather river response variable into an sf object
-  feather_river <- st_read(content(response, "text"))
+  rivers_major <- flowlines %>%
+    filter(gnis_name %in% major_names)
   
   ##Download Tigris major roads ----
   roads <- tigris::primary_roads(year = 2024)
@@ -53,12 +78,12 @@ if(API){
   # Filter roads to only those within Sacramento and Yolo Counties
   roads_filtered <- st_intersection(roads, counties_clip_boundary)
   
-  if(saveoutput == T){save(polygons, bypasses, feather_river, roads_filtered, ywa_poly,
-       file = "data/spatial/Yolo_polygon_data.Rdata")}
+  if(saveoutput == T){save(polygons, bypasses, rivers_major, roads_filtered, ywa_poly,
+       file = "data/spatial/Yolo_map_data.Rdata")}
 
 }else{
   ## Short-cut to pre saved data
-  load(file = "data/spatial/Yolo_polygon_data.Rdata")
+  load(file = "data/spatial/Yolo_map_data.Rdata")
 }
 
 ## Subset bypasses to only Yolo
@@ -70,10 +95,12 @@ ywa <- ywa_poly[ywa_poly$PROP_NAME ==  "Yolo Bypass Wildlife Area",
 
 ywa$Project_Status <- "Completed"
 colnames(ywa) <- c("project_name", "project_type", "geometry", "Project_Status")
-
+polygons <- st_transform(polygons, crs = st_crs(ywa))
 rest_polys <- rbind(polygons[,c("project_name", "project_type","Project_Status")], ywa)
 
 ## Calculate acreage of each polygon
+sf::sf_use_s2(FALSE)
+
 rest_polys$acres <- round(st_area(rest_polys)*0.00024711,1)
 ywa <- ywa_poly[ywa_poly$PROP_NAME ==  "Yolo Bypass Wildlife Area",]
 ywa$acres <- round(st_area(ywa)*0.00024711,1)
@@ -83,7 +110,7 @@ ywa$acres <- round(st_area(ywa)*0.00024711,1)
 leaflet(rest_polys) %>%
   addTiles() %>%
   addPolygons(color = "blue", weight = 1, fillOpacity = 0.5,
-              group = "rest_polys", 
+              group = "rest_polys",
               highlightOptions = highlightOptions(
     color = "red", weight = 3, fillOpacity = 0.7),
     popup = ~paste0("<b>Name:</b> ", project_name, "<br>",
@@ -91,6 +118,17 @@ leaflet(rest_polys) %>%
       "<b>Status:</b> ", Project_Status, "<br>",
       "<b>Acreage:</b> ", acres
     )) %>%
+  addCircleMarkers(
+    data = proj_pts,
+    lng = ~Lon,
+    lat = ~Lat,
+    radius = 6,
+    color = "red",
+    fillColor = "yellow",
+    fillOpacity = 0.8,
+    popup = ~paste0("<b>Project:</b> ", Site_id)
+  ) %>% 
+
   addMeasurePathToolbar(options = measurePathOptions(imperial = FALSE,
                                                      minPixelDistance = 100,
                                                      showDistances = FALSE,
@@ -144,27 +182,19 @@ if(saveoutput == T){tiff("Output/Maps/Yolo_restoration_projects_scale_%02da.tif"
 ggplot() + 
   geom_sf(data = yolo_bypass, fill = "wheat2", color = NA) +
   geom_sf(data = WW_Watershed_wgs84, fill = "slateblue3", color = "slateblue3") +
-  geom_sf(data = feather_river, color = "slateblue3") +
+  geom_sf(data = rivers_major, color = "slateblue3") +
   geom_sf(data = yolo_rest_polys[yolo_rest_polys$HRL == F, ], color = "white", fill = "forestgreen") + 
   geom_sf(data = yolo_rest_polys[yolo_rest_polys$HRL == T, ], color = "brown", fill = "orange") + theme_bw() +
   geom_sf(data = roads_filtered, color = "grey60") +
-  geom_label_repel(data = yolo_rest_polys[yolo_rest_polys$HRL == F & yolo_rest_polys$dir < 0, ], 
-                   aes(geometry = geometry, label = short_name),
-                   stat = "sf_coordinates",fill = "palegreen3", size = 2.5, force = 5, 
-                   segment.square = T, segment.inflect = T, nudge_x = -.13) +
-  geom_label_repel(data = yolo_rest_polys[yolo_rest_polys$HRL == F & yolo_rest_polys$dir > 0, ], 
-                   aes(geometry = geometry, label = short_name),
-                   stat = "sf_coordinates",fill = "palegreen3", size = 2.5, force = 5, 
-                   segment.square = T, segment.inflect = T, nudge_x = .13) +
-  geom_label_repel(data = yolo_rest_polys[yolo_rest_polys$HRL == T, ], 
-                   aes(geometry = geometry, label = short_name), 
-                   stat = "sf_coordinates",fill = "tan2", size = 2.5, nudge_x = -.1) +
+  geom_sf(data = proj_pts, color = "red", size = 3) +
   geom_label_repel(aes(x = -121.89, y = 38.511, label = "Putah Creek"), 
              data = NULL, fill = "tan2", size = 2.5) +
   geom_label_repel(aes(x = -121.79, y = 38.825, label = "Sacramento\nRiver"), 
                    data = NULL, fill = "tan2", size = 2.5, force = 0) +
   geom_label_repel(aes(x = -121.595, y = 38.825, label = "Feather\nRiver"), 
                    data = NULL, fill = "tan2", size = 2.5, force = 0) +
+  geom_label_repel( data = proj_pts, aes(geometry = geometry, label = Site_id), 
+                    stat = "sf_coordinates", size = 3, fill = "white" ) +
   coord_sf(xlim = c(-121.9, -121.4), ylim = c(38.15, 38.85), expand = FALSE) +
   annotation_scale(location = "bl", width_hint = 0.2, line_width = 1,
                    pad_x = unit(.35, "in")) + 
